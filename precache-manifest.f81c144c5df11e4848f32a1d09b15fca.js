9d603ee6062a25841eb297990a318665ffcf58e9
Technetium-s <lacina063@gmail.com>
2022-10-13 10:36:30 +0900

    Updates

---
+++ precache-manifest.f81c144c5df11e4848f32a1d09b15fca.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "fd6148b75290121ac402",
+    "url": "/static/js/main.fd6148b7.chunk.js"
+  },
+  {
+    "revision": "07156ea31df38cf958d6",
+    "url": "/static/js/1.07156ea3.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5f857e7e67121585dd20d00049e42179",
+    "url": "/index.html"
+  }
+];
